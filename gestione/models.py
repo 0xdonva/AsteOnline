249cd7e135f33ba507bd1a80bbdeabfd1d3538ea
donva <m2.violi@gmail.com>
2023-06-07 15:26:11 +0200

    Creazione del databse e migrazione

---
+++ gestione/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+# Create your models here.
+
+class Venditore(models.Model):
+
+    username = models.CharField(max_length=20, primary_key=True)
+    email = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    via = models.CharField(max_length=20)
+    numCivico = models.CharField(max_length=20)
+    CAP = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
+class Acquirente(models.Model):
+
+    username = models.CharField(max_length=20, primary_key=True)
+    email = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    via = models.CharField(max_length=20)
+    numCivico = models.CharField(max_length=20)
+    CAP = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
+class Articolo(models.Model):
+
+    # Qua andrebbe l'ID ma viene messo in automatico
+    venditore = models.ForeignKey(Venditore, on_delete=models.CASCADE)
+    titolo = models.CharField(max_length=20)
+    schedaTecnica = models.TextField()
+    immagine = models.CharField(max_length=20)
+    prezzoIniziale = models.DecimalField(max_digits=5, decimal_places=2)
+    prezzoRiserva = models.DecimalField(max_digits=5, decimal_places=2)
+    prezzoAttuale = models.DecimalField(max_digits=5, decimal_places=2)
+    dataInizioAsta = models.DateTimeField(auto_now=False, auto_now_add=False)
+    durataAsta = models.IntegerField()
+
+    def __str__(self):
+        return self.titolo
+
+class Offerta(models.Model):
+
+    acquirente = models.ForeignKey(Acquirente, on_delete=models.CASCADE)
+    articolo = models.ForeignKey(Articolo, on_delete=models.CASCADE)
+    saldo = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Offerta(models.Model):
+
+    acquirente = models.ForeignKey(Acquirente, on_delete=models.CASCADE)
+    venditore = models.ForeignKey(Venditore, on_delete=models.CASCADE)
+    testo = models.TextField()
+    voto = models.IntegerField()
--- gestione/urls.py
+++ gestione/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from gestione import urls
+
+app_name = 'gestione'
 
 urlpatterns = [
-    path('gestione/', include('gestione.urls')),
-    path('admin/', admin.site.urls),
+    
 ]
